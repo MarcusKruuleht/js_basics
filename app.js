42a9b2f2e54d2152a58ed3761204500a73d27d68
MarcusKruuleht <olen.marcus.lahe@gamil.com>
2021-09-24 11:09:08 +0300

    Fixed

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("test");
+//Muutujad
+/*
+vaatame
+kuidas
+luua ja aksutada muutujad
+*/
+
+let name = 'Kadi Tamm';
+
+name = "Kadi Lepp";
+
+let fisrtName = 'Kadi';
+let $ = 'dollar'
+
+console.log(name);
