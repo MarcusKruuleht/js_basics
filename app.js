df955df65ef740c95967bcb2a11e2edb19244dfa
MarcusKruuleht <olen.marcus.lahe@gamil.com>
2021-09-24 13:21:48 +0300

    String operations

--- app.js
+++ app.js
@@ -1,24 +1,40 @@
-const num1 = 100;
-const num2 = 50;
+const firstname = 'Kadi';
+const surname = 'Tamm';
+const age = '20';
+const text = 'Tere, olen Kadi!';
+const tags = 'arendus.veeb';
 
 let val;
+//concatenation
+val = firstname + ' ' + surname;
 
-// Matemaatilised tehed
-val = num1 + num2;
-val = num1 - num2;
-val = num1 * num2;
-val = num1 / num2;
-val = num1 % num2;
+//append
+val = firstname;
+val += surname;
+val = text + ' Minu töövaldkond on ' + tags;
 
-// Math
-val = Math.PI;
-val = Math.round(2.5);
-val = Math.ceil(2.4);
-val = Math.floor(2.6);
-val = Math.sqrt(64);
-val = Math.abs(-3);
-val = Math.pow(4, 2);
+// escaping
+val = 'See on \"jutumärkides"';
 
-val = Math.round(Math.random()*10);
+// case
+val = firstname.toLowerCase();
+val = surname.toUpperCase();
+
+val = surname[0];
+val = surname.charAt(2);
+
+val = surname.length;
+
+val = surname.charAt(surname.lenght - 1);
+
+val = surname.indexOf('a');
+val = surname.indexOf('m');
+val = surname.lastIndexOf('m');
+
+val = surname.substring(0, 2);
+val = surname.slice(0, 2);
+val = surname.slice(-3);
+
+val = tags.split(".");
 
 console.log(val);
