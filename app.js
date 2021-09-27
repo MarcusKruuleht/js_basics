8ca1cbcbf7158490dc0b45ccf35c30b734b44f84
MarcusKruuleht <olen.marcus.lahe@gamil.com>
2021-09-27 13:26:51 +0300

    Arrays and methods

--- app.js
+++ app.js
@@ -1,20 +1,42 @@
-const firstname = 'Kadi';
-const surname = 'Tamm';
-const age = '20';
-const tags = 'arendus,veeb';
+const numbers = [43, 56, 33, 23, 44, 36, 5];
+const numbers2 = new Array(22, 45, 33, 76, 54);
+const fruits = ['õun', 'banaan', 'apelsiin', 'virsik'];
+const mixed = [22 , 'Tere', true, undefined, null, {a:1, b:2}, new Date()];
 
 let val;
-//concatenation
 
+val = numbers.lenght; // massiivi pikkus
+val = Array.isArray(numbers) // kas on massiiv
+//numbers[2] = 100 // kindel väärtus
 
-// templating - es6
-val = `
-	<ul>
-		<li>Eesnimi: ${firstname}</li>
-		<li>Perenimi: ${surname}</li>
-		<li>Vanus: ${age}</li>
-		<li>Alad: ${tags}</li>
-	</ul>
-`;
-console.log(val);
-document.body.innerHTML = val;
+// lisamine
+numbers.push(250)
+numbers.unshift(320)
+
+//maha võtmine
+numbers.pop()
+numbers.shift()
+
+// index
+//val = numbers.indexOf(100)
+
+// lõikamine
+numbers.splice(3, 0, 100)
+
+// ühendamine
+val = numbers.concat(numbers2)
+
+// sorteerimine
+val = fruits.sort()
+// numbrid kasvujärjekorras
+//val = numbers.sort(function(x, y){
+//	return x - y
+//})
+
+// numbrid kahanemisjärjekorras
+//val = numbers.sort(function(x, y){
+//	return y - x
+//})
+
+console.log(numbers);
+console.log(val)
