e4b03df549d2e2c7bf9cf8ec70b629eaf3235352
MarcusKruuleht <olen.marcus.lahe@gamil.com>
2021-10-04 13:27:29 +0300

    JS events -  click events

--- app.js
+++ app.js
@@ -1,23 +1,10 @@
 // event elements
-const form = document.querySelector('form');
-const taskInput = document.querySelector('#task');
+const list = document.querySelector('ul');
 
-// keyboard
+console.log(list);
 
-//task.addEventListener('keydown', runEvent);
-//task.addEventListener('keyup', runEvent);
-//task.addEventListener('keypress', runEvent);
-
-//focus & blur
-//task.addEventListener('focus', runEvent);
-//task.addEventListener('blur', runEvent);
-
-// cut & paste
-task.addEventListener('cut', runEvent);
-task.addEventListener('paste', runEvent);
-
-// imput by symbol
-task.addEventListener('input', runEvent);
+// click by symbol
+list.addEventListener('click', runEvent);
 
 function runEvent(e){
 	console.log(`Event is ${e.type}`);
