802d075e842f230f5eee4267cccfd996d0a9f96f
MarcusKruuleht <olen.marcus.lahe@gamil.com>
2021-10-13 15:26:40 +0300

    Add and remove LS elements

--- app.js
+++ app.js
@@ -2,9 +2,19 @@
 const form = document.querySelector('form')
 const taskInput = document.querySelector('#task');
 const taskList = document.querySelector('ul');
+const clearTasks = document.getElementById('clear-tasks');
+
+
+taskList.addEventListener('click', delTask);
+
+
+// click by symbol
 
 
 form.addEventListener('submit', addTask);
+
+clearTasks.addEventListener('click', clearTask);
+
 
 function addTask(e) {
 	
@@ -18,8 +28,58 @@ function addTask(e) {
 	link.appendChild(document.createTextNode('X'))
 	link.setAttribute('href', '#');
 	li.appendChild(link);
-
 	taskList.appendChild(li);
+	
+	addTaskToLS(taskInput.value)
+
 	taskInput.value = ``;
+	
 	e.preventDefault();
+};
+
+function delTask(e){
+	if(e.target.textContent == 'X'){
+		if(confirm('Do you want to delete this task?')){
+			e.target.parentElement.remove()
+			removeTaskFromLS(e.target.parentElement.firstChild.textContent);
+		}
+	}
+
 }
+
+function clearTask(e){
+	while(taskList.firstChild){
+	taskList.removeChild(taskList.firstChild)
+	}
+
+}
+
+function addTaskToLS(task){
+	let tasks;
+	if(localStorage.getItem('tasks') === null){
+		tasks = [];
+	} else {
+		tasks = JSON.parse(localStorage.getItem('tasks'))
+	}
+	tasks.push(task);
+	localStorage.setItem('tasks', JSON.stringify(tasks))
+
+}
+
+function removeTaskFromLS(task){
+		let tasks;
+	if(localStorage.getItem('tasks') === null){
+		tasks = [];
+	} else {
+		tasks = JSON.parse(localStorage.getItem('tasks'))
+	}
+	tasks.forEach(function(tasksElement, tasksIndex){
+		if(tasksElement === task){
+			tasks.splice(tasksIndex, 1)
+		}
+	})
+
+	localStorage.setItem('tasks', JSON.stringify(tasks))
+}
+// confirm() - ok/cancel alert
+// remove() - delete element
